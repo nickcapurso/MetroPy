b7c983f38add0599cab199a3afe1428386f9ae9f
Nick Capurso <nickcapurso@gwmail.gwu.edu>
2016-04-22 21:59:07 -0400

    In some cases, the "direct" path may not be the most efficient (ex. L'Enfant to Van Dorn, you can take BL all the way, but it is not the fasted). Added some initial checks for this, more work to come.

--- AStarRail.py
+++ AStarRail.py
@@ -1,8 +1,10 @@
 import sys, getopt, requests, json
-import MetroInterface as MetroAPI
 import MetroConstants as Constants
+from MetroInterface import MetroInterface
 from MetroData import Line, Station
 from time import sleep
+
+_DEBUG = True
 
 lineInfos = {}
 stationInfos = {}
@@ -21,30 +23,38 @@ def parseApiKey(argv):
 
 def buildMetroGraph(destCode):
     global lineInfos, stationInfos, metroGraph
-    lineInfos = MetroAPI.getLineInfos(apiKey)
 
-    for line in lineInfos:
-        print(lineInfos[line])
+    if not lineInfos:
+        lineInfos = MetroAPI.getLineInfos()
 
-    stationInfos = MetroAPI.getStationInfos(apiKey)
+        if _DEBUG:
+            for line in lineInfos:
+                print(lineInfos[line])
 
-    for key in stationInfos:
-        print("Key: " + key + ", Station: " + str(stationInfos[key]))
+    if not stationInfos:
+        stationInfos = MetroAPI.getStationInfos()
 
-    for line in lineInfos:
-        lineInfos[line].mph = MetroAPI.getLineAvgSpeed(apiKey, line, lineInfos[line].startStationCode, lineInfos[line].endStationCode)
-        print("Speed (" + line +") " + str(lineInfos[line].mph))
+        if _DEBUG:
+            for key in stationInfos:
+                print("Key: " + key + ", Station: " + str(stationInfos[key]))
 
-    stationDistances = MetroAPI.getDistancesToGoal(apiKey, destCode)
+        for line in lineInfos:
+            lineInfos[line].mph = MetroAPI.getLineAvgSpeed(line, lineInfos[line].startStationCode, lineInfos[line].endStationCode)
+            
+            if _DEBUG: print("Speed (" + line +") " + str(lineInfos[line].mph))
+
+    stationDistances = MetroAPI.getDistancesToGoal(destCode)
     for station in stationDistances:
         stationInfos[station].distance = stationDistances[station]
-        print("Distance (" + station + "): " + str(stationInfos[station].distance))
+        if _DEBUG: print("Distance (" + station + "): " + str(stationInfos[station].distance))
 
     sleep(1)
     for line in lineInfos:
-        orderedList = MetroAPI.getOrderedStationList(apiKey, line, lineInfos[line].startStationCode, lineInfos[line].endStationCode)
+        orderedList = MetroAPI.getOrderedStationList(line, lineInfos[line].startStationCode, lineInfos[line].endStationCode)
         lineInfos[line].stations = orderedList
-        print("Ordered Line (" + line + ") " + str(orderedList))
+
+        if _DEBUG: print("Ordered Line (" + line + ") " + str(orderedList))
+
         for i in range(0, len(orderedList)):
             assert orderedList[i] in stationInfos
             station = stationInfos[orderedList[i]]
@@ -68,15 +78,15 @@ def buildMetroGraph(destCode):
                     if afterStation not in neighborsList:
                         neighborsList.append(afterStation)
 
-    for key in metroGraph:
-        print ("Station: " + key + ", Neighbors: " + str(metroGraph[key]))
+    if _DEBUG:
+        for key in metroGraph:
+            print ("Station: " + key + ", Neighbors: " + str(metroGraph[key]))
 
 def costToReachStation(startStation, endStation):
     minDist = 100
     minLine = ""
 
     sharedLines = set(startStation.lineList).intersection(set(endStation.lineList))
-    #print("Shared lines between " + startStation.name + " and " + endStation.name +": " + str(sharedLines))
 
     for line in sharedLines:
         stationCodeList = lineInfos[line].stations
@@ -94,16 +104,35 @@ def costToReachStation(startStation, endStation):
 
         for i in range(startIndex, endIndex):
             dist += abs(stationList[i].distance - stationList[i+1].distance)
-        #print("CTR: " + endStation.name + " from " + startStation.name + " via " + line + " = " + str(dist))
         if dist < minDist:
             minDist = dist
             minLine = line
-    print("Min Dist between " + startStation.name + " and " + endStation.name + ": " + str(minDist) + " (" + minLine + ")")
+    if _DEBUG: print("Min Dist between " + startStation.name + " and " + endStation.name + ": " + str(minDist) + " (" + minLine + ")")
     return minDist
 
 def calculateFn(startStation, endStation):
+#    if "Gallery" in endStation.name:
+#        return 100
     fn = costToReachStation(startStation, endStation) + endStation.distance
     return fn
+
+def checkShouldTakeDirectLine(queryStation, lineCode):
+    stationList = lineInfos[lineCode].stations
+    queryIndex = -1
+    for code in queryStation.codeList:
+        if code in stationList:
+            queryIndex = stationList.index(code)
+    if queryIndex == -1:
+        return False
+
+    if queryIndex == 0:
+        return queryStation.distance >= stationInfos[stationList[queryIndex+1]].distance
+    elif queryIndex == len(stationList)-1:
+        return queryStation.distance >= stationInfos[stationList[queryIndex-1]].distance
+    else:
+        return queryStation.distance > stationInfos[stationList[queryIndex-1]].distance \
+                or queryStation.distance > stationInfos[stationList[queryIndex+1]].distance
+    pass
 
 
 def findShortestPath(startCode, destCode):
@@ -114,58 +143,97 @@ def findShortestPath(startCode, destCode):
     fringe = []
     
     for line in availableLines:
-        print(lineInfos[line].stations)
+        if _DEBUG: print(lineInfos[line].stations)
         fringe += [ x for x in [stationInfos[y] for y in lineInfos[line].stations] \
                 if x not in fringe and not set(x.lineList).issubset(set(availableLines))] 
         
-    print("Initial fringe: " + str(fringe))
+    if _DEBUG: print("Initial fringe: " + str(fringe))
     for station in fringe:
         station.fn = 0 + calculateFn(currentStation, station)
-        print("F(" + station.name + "): " + str(station.fn))
+        station.parent = currentStation
+        if _DEBUG: print("F(" + station.name + "): " + str(station.fn))
 
     # While not empty
 #    while not len(fringe):
     for i in range(0,5):
-        print("Available lines: " + str(availableLines))
+        if _DEBUG: print("Available lines: " + str(availableLines))
+
         for line in availableLines:
             if destCode in lineInfos[line].stations:
-                print("On the " + line + " line")
-                return
+                if _DEBUG: print("On the " + line + " line")
+                if not checkShouldTakeDirectLine(currentStation, line):
+                    if _DEBUG: 
+                        print("But it should not be taken...")
+                else:
+                    return currentStation
 #        closestStation = min(fringe, key=lambda station: \
 #                costToReachStation(currentStation, station) + station.distance)
         closestStation = min(fringe, key=lambda station: station.fn)
-        print("Current closest station: " + str(closestStation))
+        if _DEBUG: print("Current closest station: " + str(closestStation))
 
         newLines = set(closestStation.lineList).difference(set(availableLines))
         availableLines += newLines
 #        costToReachStation(currentStation, closestStation)
 
         costSoFar = closestStation.fn - closestStation.distance
-        print("Cost so far: " + str(costSoFar))
+        if _DEBUG: print("Cost so far: " + str(costSoFar))
 
         for line in newLines:
-            print("New line to consider: " + line)
+            if _DEBUG: print("New line to consider: " + line)
             
             newNodes = [ x for x in [stationInfos[y] for y in lineInfos[line].stations] \
                     if x not in fringe and not set(x.lineList).issubset(set(availableLines))] 
 
             for station in newNodes:
                 station.fn = costSoFar + calculateFn(closestStation, station)
-                print("F(" + station.name + "): " + str(station.fn))
+                if _DEBUG: print("F(" + station.name + "): " + str(station.fn))
+                station.parent = closestStation
             fringe += newNodes
 
         currentStation = closestStation
         fringe.remove(closestStation)
-        print("Update fringe: " + str(fringe))
+        if _DEBUG: print("Update fringe: " + str(fringe))
 
-
+    
 
 assert sys.argv[1:]
 apiKey = parseApiKey(sys.argv[1:])
-"""
+startCode = input("Enter starting station code (or exit): ")
+assert startCode
+
+if "exit" in startCode:
+    exit()
+
 destCode = input("Enter destination station code: ")
 assert destCode
-"""
 
-findShortestPath("C04", "E04") # Foggy Bottom to Columbia Heights
-#buildMetroGraph("C07") # Columbia Heights
+while True:
+    MetroAPI = MetroInterface(apiKey)
+
+    if " " in startCode or " " in destCode:
+        print("Using defaults: Foggy Bottom (CO4) -> Columbia Heights (E04)")
+        startCode = "C04"
+        destCode = "E04"
+    finalStop = findShortestPath(startCode, destCode) # Foggy Bottom to Columbia Heights
+
+    pathStack = [stationInfos[destCode].name]
+    while not stationInfos[startCode].name in finalStop.name:
+        pathStack.append(finalStop.name)
+        finalStop = finalStop.parent
+    pathStack.append(stationInfos[startCode].name)
+    pathStack.reverse()
+
+    print("---------------------------------------------------")
+    print("Path:")
+    for station in pathStack:
+        print(station)
+    print("---------------------------------------------------")
+    
+    startCode = input("Enter starting station code (or exit): ")
+    assert startCode
+
+    if "exit" in startCode:
+        exit()
+
+    destCode = input("Enter destination station code: ")
+    assert destCode
--- MetroInterface.py
+++ MetroInterface.py
@@ -3,77 +3,81 @@ import MetroConstants as Constants
 from MetroData import Line,Station
 from time import sleep
 
-def getLineInfos(apiKey):
-    jsonResp = json.loads(requests.get(Constants.URL_LINES_LIST + "?api_key=" + apiKey).text)
-    lineInfos = {}
-    for i in jsonResp[Constants.LINES_TOP]:
-        displayName = i[Constants.LINES_DISPLAY_NAME]
-        lineCode = i[Constants.LINES_LINE_CODE]
-        startCode = i[Constants.LINES_START_CODE]
-        endCode = i[Constants.LINES_END_CODE]
-        lineInfos[lineCode] = Line(displayName, lineCode, startCode, endCode)
-    return lineInfos
+class MetroInterface:
+    def __init__(self, apiKey):
+        self.apiKey = apiKey
 
-def getStationInfos(apiKey):
-    jsonResp = json.loads(requests.get(Constants.URL_STATION_LIST + "?api_key=" + apiKey).text)
-    stationInfos = {}
-    for i in jsonResp[Constants.STATION_LIST_TOP]:
-        name = i[Constants.STATION_LIST_NAME]
-        code = i[Constants.STATION_LIST_CODE]
-        lc1 = i[Constants.STATION_LIST_LC1]
-        lc2 = i[Constants.STATION_LIST_LC2]
-        lc3 = i[Constants.STATION_LIST_LC3]
-        lc4 = i[Constants.STATION_LIST_LC4]
-        st1 = i[Constants.STATION_LIST_ST1]
-        st2 = i[Constants.STATION_LIST_ST2]
+    def getLineInfos(self):
+        jsonResp = json.loads(requests.get(Constants.URL_LINES_LIST + "?api_key=" +self.apiKey).text)
+        lineInfos = {}
+        for i in jsonResp[Constants.LINES_TOP]:
+            displayName = i[Constants.LINES_DISPLAY_NAME]
+            lineCode = i[Constants.LINES_LINE_CODE]
+            startCode = i[Constants.LINES_START_CODE]
+            endCode = i[Constants.LINES_END_CODE]
+            lineInfos[lineCode] = Line(displayName, lineCode, startCode, endCode)
+        return lineInfos
 
-        if name not in stationInfos:
-            station = Station(name, code, lc1, lc2, lc3, lc4, st1, st2)
-            stationInfos[name] = station
-            stationInfos[code] = station
+    def getStationInfos(self):
+        jsonResp = json.loads(requests.get(Constants.URL_STATION_LIST + "?api_key=" +self.apiKey).text)
+        stationInfos = {}
+        for i in jsonResp[Constants.STATION_LIST_TOP]:
+            name = i[Constants.STATION_LIST_NAME]
+            code = i[Constants.STATION_LIST_CODE]
+            lc1 = i[Constants.STATION_LIST_LC1]
+            lc2 = i[Constants.STATION_LIST_LC2]
+            lc3 = i[Constants.STATION_LIST_LC3]
+            lc4 = i[Constants.STATION_LIST_LC4]
+            st1 = i[Constants.STATION_LIST_ST1]
+            st2 = i[Constants.STATION_LIST_ST2]
 
-            for st in [st1, st2]:
-                if st:
-                    stationInfos[st] = station
-        else:
-            station = stationInfos[name]
-            for newLine in [lc1, lc2, lc3, lc4]:
-                if newLine:
-                    station.addLine(newLine)
-            for newCode in [st1, st2, code]:
-                if newCode:
-                    station.addStation(newCode)
-                    if newCode not in stationInfos:
-                        stationInfos[newCode] = station
-    return stationInfos
+            if name not in stationInfos:
+                station = Station(name, code, lc1, lc2, lc3, lc4, st1, st2)
+                stationInfos[name] = station
+                stationInfos[code] = station
 
-
-def getLineAvgSpeed(apiKey, lineCode, startCode, endCode):
-    jsonResp = json.loads(requests.get(Constants.URL_STATION_STATION_INFO+ "?FromStationCode=" + startCode + "&ToStationCode=" + endCode + "&api_key=" + apiKey).text)
-   
-    info = jsonResp[Constants.STATION_STATION_TOP][0]
-    totalMiles = info[Constants.STATION_STATION_MILES]
-    time = info[Constants.STATION_STATION_TIME]
-    return totalMiles * 60 / time
+                for st in [st1, st2]:
+                    if st:
+                        stationInfos[st] = station
+            else:
+                station = stationInfos[name]
+                for newLine in [lc1, lc2, lc3, lc4]:
+                    if newLine:
+                        station.addLine(newLine)
+                for newCode in [st1, st2, code]:
+                    if newCode:
+                        station.addStation(newCode)
+                        if newCode not in stationInfos:
+                            stationInfos[newCode] = station
+        return stationInfos
 
 
-def getOrderedStationList(apiKey, lineCode, startCode, endCode):
-    jsonResp = json.loads(requests.get(Constants.URL_STATION_PATH + "?FromStationCode=" + startCode + "&ToStationCode=" + endCode + "&api_key=" + apiKey).text)
+    def getLineAvgSpeed(self, lineCode, startCode, endCode):
+        jsonResp = json.loads(requests.get(Constants.URL_STATION_STATION_INFO+ "?FromStationCode=" + startCode + "&ToStationCode=" + endCode + "&api_key=" +self.apiKey).text)
+       
+        info = jsonResp[Constants.STATION_STATION_TOP][0]
+        totalMiles = info[Constants.STATION_STATION_MILES]
+        time = info[Constants.STATION_STATION_TIME]
+        return totalMiles * 60 / time
 
-    orderedStations = []
-    for i in jsonResp[Constants.STATION_PATH_TOP]:
-        orderedStations.append(i[Constants.STATION_PATH_CODE])
 
-    return orderedStations
+    def getOrderedStationList(self, lineCode, startCode, endCode):
+        jsonResp = json.loads(requests.get(Constants.URL_STATION_PATH + "?FromStationCode=" + startCode + "&ToStationCode=" + endCode + "&api_key=" +self.apiKey).text)
+
+        orderedStations = []
+        for i in jsonResp[Constants.STATION_PATH_TOP]:
+            orderedStations.append(i[Constants.STATION_PATH_CODE])
+
+        return orderedStations
 
 
-def getDistancesToGoal(apiKey, destCode):
-    jsonResp = json.loads(requests.get(Constants.URL_STATION_STATION_INFO + "?ToStationCode=" + destCode + "&api_key=" + apiKey).text)
-    stationDists = {}
+    def getDistancesToGoal(self, destCode):
+        jsonResp = json.loads(requests.get(Constants.URL_STATION_STATION_INFO + "?ToStationCode=" + destCode + "&api_key=" +self.apiKey).text)
+        stationDists = {}
 
-    for i in jsonResp[Constants.STATION_STATION_TOP]:
-        sourceStation = i[Constants.STATION_STATION_SOURCE]
-        stationDists[sourceStation] = i[Constants.STATION_STATION_MILES]
-    stationDists[destCode] = 0
-    return stationDists
+        for i in jsonResp[Constants.STATION_STATION_TOP]:
+            sourceStation = i[Constants.STATION_STATION_SOURCE]
+            stationDists[sourceStation] = i[Constants.STATION_STATION_MILES]
+        stationDists[destCode] = 0
+        return stationDists
 
